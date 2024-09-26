20e93c2944b977d0b0a45170745713cb3512bdfd
Apollo-sudo767 <blured767@gmail.com>
2024-09-26 17:50:24 -0500

    working i3

--- modules/i3.nix
+++ modules/i3.nix
@@ -1,9 +1,7 @@
 { config, pkgs, callPackage, ... }: 
 
 {
-  ...
-  environment.pathsToLink = [ "/libexec" ]; # links /libexec from derivations to /run/current-system/sw 
-  ...
+  
   services.xserver = {
     enable = true;
 
@@ -18,8 +16,9 @@
         i3status # gives you the default i3 status bar
         i3lock #default i3 screen locker
         i3blocks #if you are planning on using i3blocks over i3status
+        polybar
+        picom
      ];
     };
   };
-  ...
 }
