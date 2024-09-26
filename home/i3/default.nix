0b486c3e66ef8cefe5dc122a58df15704d4eb73a
Apollo-sudo767 <Fireshifter767@gmail.com>
2024-09-26 17:47:39 -0500

    Create default.nix

---
+++ home/i3/default.nix
@@ -0,0 +1,18 @@
+{
+  pkgs,
+  config,
+  ...
+}: {
+   # Model from hyprland stuff
+  home.file.".config/polybar/polybar.ini".source = ./polybar/polybar.ini;
+  home.file.".config/i3/i3.conf".source = ./i3.conf;
+  home.file.".config/picom/picom.conf".source =./picom.conf;
+  home.file.".config/i3/scripts" = {
+    source = ./scripts;
+    # copy the scripts directory recursively
+      recursive = true;
+      executable = true; # make all scripts executable
+  };
+  
+  
+  } 
