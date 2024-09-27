4293e36cfdc6b0c6b6e7203f286bd555bca49460
Apollo-sudo767 <blured767@gmail.com>
2024-09-26 22:04:02 -0500

    neovim test2

--- modules/neovim.nix
+++ modules/neovim.nix
@@ -1,19 +1,25 @@
-{ config, pkgs, ... };
+{ config, pkgs, ... }:
 
 {
 
   programs.neovim = {
     enable = true;
     defaultEditor = true;
-    plugins = {
-      nvim-surround
-      nvim-lspconfig
-      nvim-fzf
-      dashboard-nvim
-      lspsaga-nvim
-      feline-nvim
-      nvim-cokeline
-      nerdtree
-      vim-floaterm
-      tokyonight-nvim
   };
+  
+  
+  # Optionally install additional tools used by plugins
+  environment.systemPackages = with pkgs; [
+    fzf  # Required by fzf-vimi
+    vimPlugins.nvim-surround
+    vimPlugins.fzf-vim
+    vimPlugins.nvim-lspconfig
+    vimPlugins.dashboard-nvim
+    vimPlugins.lspsaga-nvim
+    vimPlugins.feline-nvim
+    vimPlugins.nvim-cokeline
+    vimPlugins.nerdtree
+    vimPlugins.floaterm
+    vimPlugins.tokyonight-nvim
+  ];
+}
