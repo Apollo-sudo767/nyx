2f99f26ab71b1bc77165d1c89eb9a1f31b9ff929
Apollo-sudo767 <blured767@gmail.com>
2025-02-18 15:57:48 -0600

    latest update, still a WIP

--- modules/containers.nix
+++ modules/containers.nix
@@ -9,21 +9,21 @@
     eula = true;
 
     servers = {
-    modded-survival = {
-      enable = true;
-      package = pkgs.fabricServers.fabric-1_20_1;
+      modded-survival = {
+        enable = true;
+        package = pkgs.fabricServers.fabric-1_20_1;
 
-      symlinks = 
-      let 
-        modpack = {pkgs.fetchPackwizModpack {
-          url = "https://drive.google.com/file/d/1_wh-wSRD8_36-oXzygCsyvdYsyc-RNTQ/edit";
-          packHash = "sha256-88c7baafd88effb5411f9f64db696a068680fec7e50e02eb00a37e03ebf83601";
-  }}
-      in
-      {
-        "mods" = "${modpack}/mods";
-      }
+        symlinks = let 
+          modpack = pkgs.fetchPackwizModpack {
+            url = "https://drive.google.com/file/d/1_wh-wSRD8_36-oXzygCsyvdYsyc-RNTQ/edit";
+            packHash = "sha256-88c7baafd88effb5411f9f64db696a068680fec7e50e02eb00a37e03ebf83601";
+          };
+        in
+        {
+          "mods" = "${modpack}/mods";
+        };
+        };
+      };
     };
-  };
-  };
-  }
+}
+
